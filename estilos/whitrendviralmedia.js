c623ab4eb10ada496a1d8cf7a390c6aa4e00f5b4
MarcosManzino <marcosmanzino01@gmail.com>
2023-06-16 22:17:27 -0300

    03

--- estilos/whitrendviralmedia.js
+++ estilos/whitrendviralmedia.js
@@ -3,12 +3,28 @@ const btn2 = document.querySelector('#btn2');
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 
+let currentImage = 'img1';
+
 btn1.addEventListener('click', () => {
-  img1.style.display = 'block';
-  img2.style.display = 'none';
+  if (currentImage === 'img2') {
+    currentImage = 'img1';
+    animateImages(img2, img1);
+  }
 });
 
 btn2.addEventListener('click', () => {
-  img1.style.display = 'none';
-  img2.style.display = 'block';
+  if (currentImage === 'img1') {
+    currentImage = 'img2';
+    animateImages(img1, img2);
+  }
 });
+
+function animateImages(hideImg, showImg) {
+  hideImg.style.opacity = '0';
+  showImg.style.opacity = '1';
+  
+  setTimeout(() => {
+    hideImg.style.zIndex = '1';
+    showImg.style.zIndex = '2';
+  }, 1000);
+}
